7bf1fa10e51f31d859f57d54849639ac31e76e31
carrymartes <carrymartes@gmail.com>
2021-07-09 14:06:25 +0600

    first commit

---
+++ dist/js/bundle.js
@@ -0,0 +1,3 @@
+/*! For license information please see bundle.js.LICENSE.txt */
+(()=>{var e={r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},o={};console.log("asi"),(()=>{"use strict";e.r(o)})()})();
+//# sourceMappingURL=bundle.js.map
